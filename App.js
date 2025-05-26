e196149a193b615a61c4d618bb74c91c909cdacd
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2025-05-26 14:12:16 +0500

    Student Form

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 let add = document.getElementById("add")
-    let first = document.getElementById('first')
-    let arr = JSON.parse(localStorage.getItem('studentData')) || []
+let first = document.getElementById('first')
+let arr = JSON.parse(localStorage.getItem('studentData')) || []
 
-    add.addEventListener("click", function () {
-      first.innerHTML = `
+add.addEventListener("click", function () {
+  first.innerHTML = `
         <input type="text" placeholder="Enter Your Name" id="name"><br>
         <input type="email" placeholder="Enter Your Email" id="email" required><br>
         <input type="number" placeholder="Enter Your Age" id="age"><br>
         <input type="number" placeholder="Enter A Mobile Number" id="mobile"><br>
         <input type="radio" value="Male" name="gender" class="abc" id="male"><label for="male" id="">Male</label>
         <input type="radio" value="Female" name="gender" class="abc" id="female"><label for="female" id="">Female</label>  <br>
-        <button id="second" class="btn btn-success mt-3">Add Student</button>
-        <button id="clear" class="btn btn-danger mt-3">Delete All Data</button>
-        <button id="checkStudent" class="btn btn-info mt-3 text-white">Check Student</button>
+        <button id="second" class="btn1 mt-3">Add Student</button>
+        <button id="clear" class="btn1 mt-3">Delete All Data</button>
+        <button id="checkStudent" class="btn1  mt-3 ">Check Student</button>
         <br><br>
-        <table class="table table-bordered text-white">
+        <table class="table-bordered text-white">
           <thead>
             <tr>
               <th>Roll Number</th>
@@ -30,73 +30,91 @@ let add = document.getElementById("add")
           <tbody id="table"></tbody>
         </table>
       `
-      document.getElementById('checkStudent').addEventListener('click', () => {
-        window.location.href = `student.html`
-      })
+  document.getElementById('checkStudent').addEventListener('click', () => {
+    window.location.href = `student.html`
+  })
 
-      let table = document.getElementById("table")
-      document.getElementById('clear').addEventListener('click', () => {
-        localStorage.clear();
-        arr = [];
-        table.innerHTML = ``;
-      })
+  let table = document.getElementById("table")
+  document.getElementById('clear').addEventListener('click', () => {
+    localStorage.clear();
+    arr = [];
+    table.innerHTML = ``;
+  })
 
-      add.style.display = 'none'
-      let mob = document.getElementById("mobile")
-      let email = document.getElementById("email")
-      let age = document.getElementById("age")
-      let StudentName = document.getElementById("name")
-      let gender = document.getElementsByName('gender')
+  add.style.display = 'none'
+  let mob = document.getElementById("mobile")
+  let email = document.getElementById("email")
+  let age = document.getElementById("age")
+  let StudentName = document.getElementById("name")
+  let gender = document.getElementsByName('gender')
+  let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  email.addEventListener('change', () => {
+    if (regex.test(email.value)) {
+      email.style.border = `3px solid green`
+    } else {
 
-      document.getElementById("second").addEventListener('click', function () {
-        let ans;
-        for (let i = 0; i < gender.length; i++) {
-          if (gender[i].checked) {
-            ans = gender[i].value
-          }
-        }
+      email.style.border = `3px solid red`
+      alert('Please Enter Correct Email!')
+    }
+  })
+  let pkNumberRegex = /^(\+92|0092|92|0)3[0-9]{9}$/;
+mob.addEventListener('change', () => {
+    if (pkNumberRegex.test(mob.value)) {
+      mob.style.border = `3px solid green`
+    } else {
+      mob.style.border = `3px solid red`
+      alert('Please Enter Correct Number!')
+    }
+  })
+  document.getElementById("second").addEventListener('click', function () {
+    let ans;
+    for (let i = 0; i < gender.length; i++) {
+      if (gender[i].checked) {
+        ans = gender[i].value
+      }
+    }
 
-        if (!StudentName.value || !email.value || !age.value || !mob.value || !ans) {
-          alert("Please fill all fields!");
-          return;
-        }
+    if (!StudentName.value || !email.value || !age.value || !mob.value || !ans) {
+      alert("Please fill all fields!");
+      return;
+    }
 
-        let obj = {
-          RollNumber: Math.floor(Math.random() * 1000 + 1000),
-          name: StudentName.value.charAt(0).toUpperCase() + StudentName.value.slice(1),
-          gender: ans,
-          age: age.value,
-          email: email.value,
-          mobile: mob.value,
-          Date: dayjs().format(),
-        }
-        arr.push(obj)
+    let obj = {
+      RollNumber: Math.floor(Math.random() * 1000 + 1000),
+      name: StudentName.value.charAt(0).toUpperCase() + StudentName.value.slice(1),
+      gender: ans,
+      age: age.value,
+      email: email.value,
+      mobile: mob.value,
+      Date: dayjs().format(),
+    }
+    arr.push(obj)
 
-        mob.value = ``
-        StudentName.value = ``
-        age.value = ``
-        email.value = ``
-        local()
-      })
+    mob.value = ``
+    StudentName.value = ``
+    age.value = ``
+    email.value = ``
+    local()
+  })
 
-      local()
-    })
+  local()
+})
 
-    function local() {
-      let table = document.getElementById("table")
-      table.innerHTML = ``
-      localStorage.setItem('studentData', JSON.stringify(arr))
-      for (let i = 0; i < arr.length; i++) {
-        let now = dayjs();
-        let date = dayjs(arr[i].Date);
-        let diff = now.diff(date, 'second');
-        let result =
-          diff < 60 ? `${diff} seconds ago` :
-            diff < 3600 ? `${Math.floor(diff / 60)} minutes ago` :
-              diff < 86400 ? `${Math.floor(diff / 3600)} hours ago` :
-                `${Math.floor(diff / 86400)} days ago`;
+function local() {
+  let table = document.getElementById("table")
+  table.innerHTML = ``
+  localStorage.setItem('studentData', JSON.stringify(arr))
+  for (let i = 0; i < arr.length; i++) {
+    let now = dayjs();
+    let date = dayjs(arr[i].Date);
+    let diff = now.diff(date, 'second');
+    let result =
+      diff < 60 ? `${diff} seconds ago` :
+        diff < 3600 ? `${Math.floor(diff / 60)} minutes ago` :
+          diff < 86400 ? `${Math.floor(diff / 3600)} hours ago` :
+            `${Math.floor(diff / 86400)} days ago`;
 
-        table.innerHTML += `
+    table.innerHTML += `
           <tr>
             <td>${arr[i].RollNumber}</td>
             <td>${arr[i].name}</td>
@@ -107,14 +125,14 @@ let add = document.getElementById("add")
             <td>${result}</td>
             <td><img src="https://img.icons8.com/ios-glyphs/30/ffffff/delete-sign.png" alt="delete" class="dlt" width="25"></td>
           </tr>`
-      }
+  }
 
-      let dlt = document.querySelectorAll('.dlt')
-      dlt.forEach((icon, index) => {
-        icon.addEventListener('click', () => {
-          arr.splice(index, 1)
-          localStorage.setItem("studentData", JSON.stringify(arr));
-          local()
-        })
-      })
-    }
+  let dlt = document.querySelectorAll('.dlt')
+  dlt.forEach((icon, index) => {
+    icon.addEventListener('click', () => {
+      arr.splice(index, 1)
+      localStorage.setItem("studentData", JSON.stringify(arr));
+      local()
+    })
+  })
+}
